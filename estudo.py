a91c99330188d60d7ca3bc275e296386f569dc93
AlvaroTF-git <alvaro.tferreir4@gmail.com>
2025-06-26 00:25:42 +0000

    feito

--- estudo.py
+++ estudo.py
@@ -1,7 +1,20 @@
 print('========= Programa para aprovação de empréstimos bancários ========')
-name = str('Olá, por favor informe o seu nome completo: ')
+name = str(input('Olá, por favor informe o seu nome completo: '))
 money = float(input('Digite aqui o seu salário mensal: '))
 house = float(input('Diga o preço da casa na qual deseja comprar: '))
 years = int(input('Em quantos anos o senhor pretente pagar a casa? '))
-prestacao = float(money / 0.30)
-print(money)
+condition = float(money * 0.30)
+months = years * 12
+prestacao = house / months
+if prestacao > condition:
+    print('Perdão, infelizmente você não pode financiar a casa!')
+elif prestacao < condition:
+    print("Parabéns,",name,",Você está apto para financiar a casa!")
+else:
+    print("Algo ocorreu errado, rode o programa novamente!")
+
+
+
+
+
+    
